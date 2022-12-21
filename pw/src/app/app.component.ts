f5c827b5cc33c95b443a5338269c080c7436746c
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-21 23:53:10 +0700

    Tracking input state

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,9 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  includeCharacters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
   password: string = 'initial password';
   onButtonClick(): void {
     this.password = 'MY PASSWORD!!!!';
+  }
+
+  onChangeUseCharacters(): void {
+    this.includeCharacters = !this.includeCharacters;
+    console.log(`include characters: ${this.includeCharacters}`);
+  }
+
+  onChangeUseNumbers(): void {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(): void {
+    this.includeSymbols = !this.includeSymbols;
   }
 
   getName(): string {
