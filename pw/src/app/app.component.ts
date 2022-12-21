ea2c4156f71f6db10f6dcf5bc365d34e65a3b606
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-22 01:07:03 +0700

    Generated random password

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -9,14 +9,48 @@ export class AppComponent {
   includeCharacters: boolean = false;
   includeNumbers: boolean = false;
   includeSymbols: boolean = false;
+  length: number = 0;
   password: string = 'initial password';
+
   onButtonClick(): void {
-    this.password = 'MY PASSWORD!!!!';
+    const numbers: string = '0123456789';
+    const characters: string = 'abcdefghijklmnopqrstuvwyz';
+    const symbols: string = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeCharacters) {
+      validChars += characters;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let randomIndex: number = 0;
+    let generatedPassword: string = '';
+    for (let i = 0; i < this.length; ++i) {
+      randomIndex = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[randomIndex];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  onInputLength(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    console.log(`inputLength: ${target.value}`);
+    const parseNumber = Number.parseInt(target.value);
+    if (!isNaN(parseNumber)) {
+      this.length = parseNumber;
+    }
   }
 
   onChangeUseCharacters(): void {
     this.includeCharacters = !this.includeCharacters;
-    console.log(`include characters: ${this.includeCharacters}`);
   }
 
   onChangeUseNumbers(): void {
