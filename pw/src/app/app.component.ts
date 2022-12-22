e846b8387fbe52f59ede7263acdd19f8b4aa433a
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-22 16:40:24 +0700

    Added disabled btn if doesn't meet requirements

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -11,6 +11,21 @@ export class AppComponent {
   includeSymbols: boolean = false;
   length: number = 0;
   password: string = 'initial password';
+  isEnableBtn: boolean = false;
+
+  checkEnableGenerateBtn(): void {
+    if (this.length > 0) {
+      if (
+        this.includeNumbers ||
+        this.includeCharacters ||
+        this.includeSymbols
+      ) {
+        this.isEnableBtn = true;
+      } else this.isEnableBtn = false;
+    } else {
+      this.isEnableBtn = false;
+    }
+  }
 
   onButtonClick(): void {
     const numbers: string = '0123456789';
@@ -46,19 +61,23 @@ export class AppComponent {
     const parseNumber = Number.parseInt(target.value);
     if (!isNaN(parseNumber)) {
       this.length = parseNumber;
-    }
+    } else this.length = 0;
+    this.checkEnableGenerateBtn();
   }
 
   onChangeUseCharacters(): void {
     this.includeCharacters = !this.includeCharacters;
+    this.checkEnableGenerateBtn();
   }
 
   onChangeUseNumbers(): void {
     this.includeNumbers = !this.includeNumbers;
+    this.checkEnableGenerateBtn();
   }
 
   onChangeUseSymbols(): void {
     this.includeSymbols = !this.includeSymbols;
+    this.checkEnableGenerateBtn();
   }
 
   getName(): string {
