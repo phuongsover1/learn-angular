d251598826315fd7d5fd9f0e777ccbfd7fce1ab9
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-22 16:49:06 +0700

    Changed to the simple logic for disable button instead of the complex's one implemented before

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -11,21 +11,6 @@ export class AppComponent {
   includeSymbols: boolean = false;
   length: number = 0;
   password: string = 'initial password';
-  isEnableBtn: boolean = false;
-
-  checkEnableGenerateBtn(): void {
-    if (this.length > 0) {
-      if (
-        this.includeNumbers ||
-        this.includeCharacters ||
-        this.includeSymbols
-      ) {
-        this.isEnableBtn = true;
-      } else this.isEnableBtn = false;
-    } else {
-      this.isEnableBtn = false;
-    }
-  }
 
   onButtonClick(): void {
     const numbers: string = '0123456789';
@@ -62,22 +47,18 @@ export class AppComponent {
     if (!isNaN(parseNumber)) {
       this.length = parseNumber;
     } else this.length = 0;
-    this.checkEnableGenerateBtn();
   }
 
   onChangeUseCharacters(): void {
     this.includeCharacters = !this.includeCharacters;
-    this.checkEnableGenerateBtn();
   }
 
   onChangeUseNumbers(): void {
     this.includeNumbers = !this.includeNumbers;
-    this.checkEnableGenerateBtn();
   }
 
   onChangeUseSymbols(): void {
     this.includeSymbols = !this.includeSymbols;
-    this.checkEnableGenerateBtn();
   }
 
   getName(): string {
