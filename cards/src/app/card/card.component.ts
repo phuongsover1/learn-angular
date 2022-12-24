d64d631afe017bc00e5e93620d1aedf2cbba2847
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-24 16:59:49 +0700

    Accepting Data in a Child Component & display in child's template

--- cards/src/app/card/card.component.ts
+++ cards/src/app/card/card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  @Input() title: string = '';
+  @Input() imageUrl: string = '';
+  @Input() username: string = '';
+  @Input() content: string = '';
 }
