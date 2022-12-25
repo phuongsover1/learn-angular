02c3ad9a17e77d161dab35872321e67a7795801a
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-25 19:56:57 +0700

    Randomly Generating text & display content

--- typing-challenge/src/app/app.component.ts
+++ typing-challenge/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  title: string = 'typing-challenge';
+  length: number = 10;
+  randomText: string = faker.lorem.paragraph();
+  isSuccess: boolean = false;
+
+  public generateRandomText(): void {
+    this.randomText = faker.lorem.paragraph();
+  }
 }
