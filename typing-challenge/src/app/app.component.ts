4f5cdf6c1290173a50ab352d8fe7b6f34eaf9a94
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-25 20:32:59 +0700

    Handling user input & conditional display

--- typing-challenge/src/app/app.component.ts
+++ typing-challenge/src/app/app.component.ts
@@ -10,8 +10,28 @@ export class AppComponent {
   title: string = 'typing-challenge';
   randomText: string = faker.lorem.sentence();
   isSuccess: boolean = false;
+  recentTypingValue: string = '';
+
+  target: HTMLInputElement | null = null;
 
   public generateRandomText(): void {
+    console.log(this.target);
+
+    if (this.target) {
+      this.target.disabled = false;
+      this.target.value = '';
+    }
     this.randomText = faker.lorem.sentence();
+    this.isSuccess = false;
+  }
+
+  public onInputTypingChange(e: Event) {
+    if (!this.target) this.target = e.target as HTMLInputElement;
+    this.recentTypingValue = this.target.value;
+
+    if (this.recentTypingValue === this.randomText) {
+      this.isSuccess = true;
+      this.target.disabled = true;
+    }
   }
 }
