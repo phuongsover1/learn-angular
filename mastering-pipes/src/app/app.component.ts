1ad7ec8cf732460634616b65f3032e2fe80c9fec
phuongsover1 <phuongsover2@gmail.com>
2022-12-27 22:46:46 +0700

    Added Date Picker Input.

--- mastering-pipes/src/app/app.component.ts
+++ mastering-pipes/src/app/app.component.ts
@@ -8,10 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mastering-pipes';
   name: string = '';
+  date: string = '';
 
   onNameChange(event: Event) {
     const target = event.target as HTMLInputElement;
     const value: string = target.value;
     this.name = value;
+  }
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.date = target.value;
   }
 }
