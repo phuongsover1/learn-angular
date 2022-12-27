5f74baaa020f53af7a275565cd06326ba3192c1c
phuongsover1 <phuongsover2@gmail.com>
2022-12-27 18:42:56 +0700

    Implemented character by Character comparision logic.

--- typing-challenge/src/app/app.component.ts
+++ typing-challenge/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { faker } from '@faker-js/faker';
 
+interface ColorCharacter {
+  character: string;
+  color: 'red' | 'green' | 'gray';
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,21 +16,22 @@ export class AppComponent {
   randomText: string = faker.lorem.sentence();
   isSuccess: boolean = false;
   recentTypingValue: string = '';
-
+  reset: boolean = true;
   target: HTMLInputElement | null = null;
+  checkCharacterList: ColorCharacter[] = [];
 
   public generateRandomText(): void {
-    console.log(this.target);
-
     if (this.target) {
       this.target.disabled = false;
       this.target.value = '';
     }
     this.randomText = faker.lorem.sentence();
     this.isSuccess = false;
+    this.reset = true;
   }
 
   public onInputTypingChange(e: Event) {
+    this.reset = false;
     if (!this.target) this.target = e.target as HTMLInputElement;
     this.recentTypingValue = this.target.value;
 
@@ -33,5 +39,26 @@ export class AppComponent {
       this.isSuccess = true;
       this.target.disabled = true;
     }
+
+    this.checkCharacterList = [];
+
+    for (let i = 0; i < this.recentTypingValue.length; ++i) {
+      if (this.recentTypingValue[i] === this.randomText[i]) {
+        this.checkCharacterList.push({
+          character: this.recentTypingValue[i],
+          color: 'green',
+        });
+      } else
+        this.checkCharacterList.push({
+          character: this.randomText[i],
+          color: 'red',
+        });
+    }
+
+    // color gray the remain of random text when user doesn't typing yet
+    this.checkCharacterList.push({
+      character: this.randomText.substring(this.recentTypingValue.length),
+      color: 'gray',
+    });
   }
 }
