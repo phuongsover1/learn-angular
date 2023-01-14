27fc20efdfec79986d4d7a05ebdad643046fd4c7
Phuong Nguyen <phuongsover2@gmail.com>
2023-01-14 10:22:01 +0700

    passing custom arguments to custom pipe.

--- mastering-pipes/src/app/convert.pipe.ts
+++ mastering-pipes/src/app/convert.pipe.ts
@@ -1,10 +1,11 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
   transform(value: unknown, ...args: unknown[]): string | number | undefined {
+    console.log(args);
     if (!value) return '';
     if (typeof value === 'number' && !isNaN(value)) return 1.609344 * value;
     return undefined;
