78eb5f8eaf32cec42a6a5a401a65bba04b84c54c
Phuong Nguyen <phuongsover2@gmail.com>
2023-01-14 12:48:17 +0700

    using ngClass to add class dynamically.

---
+++ pages/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+
+interface Image {
+  title: string;
+  url: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'pages';
+  currentPage: number = 0;
+  images: Image[] = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    }, {
+      title: 'At the Forest',
+      url: 'https://images.unsplash.com/photo-1473448912268-2022ce9509d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    }, {
+      title: 'At the City',
+      url: 'https://images.unsplash.com/photo-1449824913935-59a10b8d2000?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2l0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the Snow',
+      url: 'https://images.unsplash.com/photo-1483921020237-2ff51e8e4b22?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c25vd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    }
+  ]
+}
