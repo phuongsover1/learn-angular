5f015e199163b50bef79b8f4336eb06c240cce3e
Phuong Nguyen <phuongsover2@gmail.com>
2023-02-01 22:44:29 +0700

    replace *ngFor to custom ngFor-like directive.

--- pages/src/app/times.directive.ts
+++ pages/src/app/times.directive.ts
@@ -14,7 +14,9 @@ export class TimesDirective {
 
     console.log(times)
     for (let i = 0; i < times; ++i) {
-      this.viewContainer.createEmbeddedView(this.templateRef, {});
+      this.viewContainer.createEmbeddedView(this.templateRef, {
+        index: i,
+      });
     }
   }
 }
